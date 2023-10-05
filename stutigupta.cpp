dfd5491e0e205419dd2a975f6edd6bfc92f9b0f0
StutiGupta12 <146556895+StutiGupta12@users.noreply.github.com>
2023-10-05 20:28:11 +0530

    perimeter & area user choice switch

--- stutigupta.cpp
+++ stutigupta.cpp
@@ -1,77 +1,35 @@
 #include<stdio.h>
 void main()
 {
-	int ch, c;
-	float l, b, r, s, a, p;
-	printf("MENU\n\t1.Area \n\t2. Perimeter");
-	printf("\nEnter your choice from 1 to 2");
-	scanf_s("%d", &ch);
-	if (ch == 1)
-	{
-		printf("MENU\n\t1.Square \n\t2.Rectangle\n\t3.Circle\nEnter your choice from 1 to 3");
-		scanf_s("%d", &c);
-		if (c == 1)
-		{
-			printf("Enter side of square");
-			scanf_s("%f", &s);
-			a = s * s;
-			printf("Area is %.2f", a);
-		}
-		else if (c == 2)
-		{
-			printf("Enter length and breadth");
-			scanf_s("%f%f", &l, &b);
-			a = l* b;
-			printf("Area is %.2f", a);
-		}
-		else if (c == 3)
-		{
-			printf("Enter radius of circle");
-			scanf_s("%f", &r);
-			a = 3.14 * r * r;
-			printf("Area is %f", a);
-		}
-		else
-
-		{
-
-			printf("try again");
-		}
-	}
-	else if (ch == 2)
-	{
-		printf("MENU\n\t1.Square\n\t2.Rectangle\n\t3.Circle\nEnter your choice from 1 to 3");
-		scanf_s("%d", &c);
-		if (c == 1)
-		{
-			printf("Enter side of square");
-			scanf_s("%f", &s);
-			p = 4 * s;
-			printf("Perimeter is %.2f", p);
-		}
-		else if (c==2)
-		{
-			printf("Enter length and breadth");
-			scanf_s("%f%f", &l, &b);
-			p = 2 * (1 + b);
-			printf("Perimeter is %f", p);
-		}
-		else if (c==3)
-		{
-			printf("Enter radius of circle");
-			scanf_s("%f", &r);
-			p = 2 * 3.14 * r;
-			printf("Perimeter is %f", p);
-		}
-		else
-		{
-			printf("try again");
-		}
-
-	}
-	else
-	{
-		printf("try again");
-
-	}
+    int ch;
+    float l, b, s, r, a, p;
+    printf("MENU:\n\t1.Square\n\t2.Circle\n\t3.Rectangle");
+    printf("\nEnter your choice from 1-3");
+    scanf_s("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+        printf("Enter side of square");
+        scanf_s("%f", &s);
+        p = 4 * s;
+        a = s * s;
+        printf("Area of square is %.2f\nPerimeter of square is %.2f", a, p);
+        break;
+    case 2:
+        printf("Enter radius of circle");
+        scanf_s("%f", &r);
+        p = 2 * 3.14 * r;
+        a = 3.14 * r * r;
+        printf("Area of circle is %.2f\nPerimeter of circle is %.2f", a, p);
+        break;
+    case 3:
+        printf("Enter length and breadth of rectangle");
+        scanf_s("%f%f", &l, &b);
+        p = 2 * (l + b);
+        a = l * b;
+        printf("Area of rectangle is %.2f\nPerimeter of rectangle is %.2f", a, p);
+        break;
+    default:
+        printf("Enter Number from 1-3");
+    }
 }
