4fb5287aac0b4ce07781d657996f3ae54a0dc291
StutiGupta12 <146556895+StutiGupta12@users.noreply.github.com>
2023-12-01 00:05:48 +0530

    Calculator using switch and func

--- array.c
+++ array.c
@@ -1,33 +1,59 @@
 #include <stdio.h>
-#include <string.h>
+int add(int x,int y)
+{
+    int z=x+y;
+    return z;
+}
+int sub(int a,int b)
+{
+    int c=a-b;
+    return c;
+}
+int mult(int d,int e)
+{
+    int f=d*e;
+    return f;
+}
+int div(int g,int h)
+{
+    if(h!=0)
+    {
+    int i=g/h;
+    return i;
+    }
+    else
+    {
+        printf("Error denominator cannot be zero");
+        return 0;
+    }
+}
 void main()
 {
-    char str[20],str1[20];
-    int l,i,j,flag=0;
-    printf("\nEnter the string :- ");
-    gets(str);
-    l = strlen(str);
-    for(i=0,j=l-1;i<l;i++,j--)
+    int a,b,ch,x,y,z,f;
+    printf("Menu:\n1.Add\n2.Subtract\n3.Multiplication\n4.Division\n");
+    printf("Enter your choice from 1 to 4");
+    scanf("%d",&ch);
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    switch(ch)
     {
-        str1[j]=str[i];
-    }
-    for(i=0;i<l;i++)
-    {
-        if(str1[i]==str[i])
-        {
-            flag=0;
-        }
-        else
-        {
-            flag=-1;
-            break;
-        }
-    }
-    if(flag==0)
-    {
-        printf("String is pallindrome");
-    }
-    else{
-        printf("String is not pallindrome");
+        case 1:
+        x=add(a,b);
+        printf("Sum is %d",x);
+        break;
+        case 2:
+        y=sub(a,b);
+        printf("Subtraction is %d",y);
+        break;
+        case 3:
+        z=mult(a,b);
+        printf("Product is %d",z);
+        break;
+        case 4:
+        f=div(a,b);
+        printf("Quotient is %d",f);
+        break;
+        default:
+        printf("Error!Try agan by entering choice from 1 to 4");
     }
 }
