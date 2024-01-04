10918cd73a3ced3484ad1a379b7414166d444fc3
StutiGupta12 <146556895+StutiGupta12@users.noreply.github.com>
2024-01-05 00:26:28 +0530

    operationon marks with array of structures

--- array.c
+++ array.c
@@ -1,23 +1,58 @@
-#include<stdio.h>
+#include <stdio.h>
 
-union info {
-    int m, r;
+// Define a structure to represent student information
+struct Student {
     char name[50];
+    int rollNumber;
+    float marks;
 };
 
+// Function to display student information
+void displayStudent(struct Student s) {
+    printf("Name: %s\n", s.name);
+    printf("Roll Number: %d\n", s.rollNumber);
+    printf("Marks: %.2f\n", s.marks);
+    printf("\n");
+}
+
+// Function to calculate and display average marks of all students
+void calculateAverage(struct Student students[], int size) {
+    float totalMarks = 0;
+
+    for (int i = 0; i < size; i++) {
+        totalMarks += students[i].marks;
+    }
+
+    float average = totalMarks / size;
+    printf("Average Marks of All Students: %.2f\n", average);
+}
+
 int main(void) {
-    union info i1;
+    // Declare an array of structures to store student marks
+    struct Student students[3];
 
-    printf("Enter name: ");
-    scanf("%s", i1.name);
+    // Input information for each student
+    for (int i = 0; i < 3; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
 
-    printf("Enter roll number: ");
-    scanf("%d", &i1.r);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].rollNumber);
 
-    printf("Enter marks: ");
-    scanf("%d", &i1.m);
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
 
-    printf("Name: %s\nRoll Number: %d\nMarks: %d\n", i1.name, i1.r, i1.m);
+        printf("\n");
+    }
+
+    // Display information for each student using the displayStudent function
+    for (int i = 0; i < 3; i++) {
+        displayStudent(students[i]);
+    }
+
+    // Calculate and display the average marks of all students using the calculateAverage function
+    calculateAverage(students, 3);
 
     return 0;
 }
