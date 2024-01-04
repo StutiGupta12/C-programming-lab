b42f7b500028350404068a48619725b7a51c20fc
StutiGupta12 <146556895+StutiGupta12@users.noreply.github.com>
2024-01-04 23:44:07 +0530

    marks sum using structures

--- array.c
+++ array.c
@@ -1,36 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    
-    int *arr1 = (int*)malloc(sizeof(int));
-    
-    if (arr1 == NULL) {
-        printf("Memory allocation failed for arr1\n");
-        return 1;
-    }
-
-    *arr1 = 42;
-    printf("arr1 value: %d\n", *arr1);
-    arr1 = (int*)realloc(arr1, 2 * sizeof(int));
-
-    if (arr1 == NULL) {
-        printf("Memory reallocation failed for arr1\n");
-        return 1;
-    }
-    arr1[1] = 100;
-    printf("arr1 values: %d, %d\n", arr1[0], arr1[1]);
-    int *arr2 = (int*)calloc(3, sizeof(int));
-
-    if (arr2 == NULL) {
-        printf("Memory allocation failed for arr2\n");
-        return 1;
-    }
-
-    printf("arr2 values: %d, %d, %d\n", arr2[0], arr2[1], arr2[2]);
-    free(arr1);
-    free(arr2);
-
-    return 0;
+#include<stdio.h>
+struct info
+{
+    int m1,m2,m3,tm;
+};
+void main()
+{
+struct info i1;
+printf("Enter marks of first subject");
+scanf("%d",&i1.m1);
+printf("Enter marks of second subject");
+scanf("%d",&i1.m2);
+printf("Enter marks of third subject");
+scanf("%d",&i1.m3);
+i1.tm=i1.m1+i1.m2+i1.m3;
+printf("Sum of marks is %d",i1.tm);
 }
-
